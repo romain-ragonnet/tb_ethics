12a8bb41378fd6c2cdc72a08a150a7cef1ffca13
Romain Ragonnet <romain.ragonnet@monash.edu>
2024-04-04 15:52:22 +1100

    Refactor creating wrapper functions

--- model/calibration.py
+++ model/calibration.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import yaml
+from pathlib import Path
+from matplotlib import pyplot as plt
 
+from estival.wrappers.nevergrad import optimize_model
 from estival.model import BayesianCompartmentalModel
 import estival.priors as esp
 import estival.targets as est
 
 from model.model import build_model
+
+FIXED_PARAMS_PATH = Path(__file__).parent.resolve() / "fixed_params.yml"
 
 
 def get_calibration_bcm(fixed_params, target_incidence=100) -> BayesianCompartmentalModel:
@@ -22,3 +28,28 @@ def get_calibration_bcm(fixed_params, target_incidence=100) -> BayesianCompartme
     ]
 
     return BayesianCompartmentalModel(model, fixed_params, priors, targets)
+
+
+def fit_model(target_incidence=200): 
+    with open(FIXED_PARAMS_PATH, "r") as f:
+        fixed_params = yaml.safe_load(f)
+    bcm = get_calibration_bcm(fixed_params, target_incidence=200)    
+    orunner = optimize_model(bcm, num_workers=8)
+    rec = orunner.minimize(1000)
+    mle_params = rec.value[1]
+
+    return bcm, mle_params
+
+
+def check_fit(bcm, mle_params):
+    baseline_decision_vars = {
+        "decision_var_trans": 0.,
+        "decision_var_cdr": 0.,
+        "decision_var_pt": 0.
+    }
+    res = bcm.run(mle_params | baseline_decision_vars)
+    derived_df = res.derived_outputs
+    derived_df['incidence_per100k'].plot()
+
+    target_data = bcm.targets['incidence_per100k'].data
+    plt.scatter(target_data.index, target_data, color='red')
--- model/optimisation.py
+++ model/optimisation.py
@@ -1,23 +1,26 @@
 import pandas as pd
 from jax import numpy as jnp
+import yaml
 
+from estival.wrappers.nevergrad import optimize_model
 from estival.model import BayesianCompartmentalModel
 import estival.priors as esp
 import estival.targets as est
 
 from model.model import build_model
+from model.calibration import FIXED_PARAMS_PATH
 
 
-def get_optimisation_bcm(fixed_params, decision_var_sum_threshold= 1.5) -> BayesianCompartmentalModel:
+def get_optimisation_bcm(interv_params, decision_var_sum_threshold= 1.5, minimised_indicator="incidence_per100k") -> BayesianCompartmentalModel:
     """
     Constructs and returns a Bayesian Compartmental Model object for the purpose of intervention optimisation.
     """
-    model = build_model(fixed_params)
+    model = build_model(interv_params)
 
     priors =  [esp.UniformPrior(f"decision_var_{intervention}", (0, 1.)) for intervention in ['trans', 'cdr', 'pt']]
 
     targets = [
-        est.NormalTarget("incidence_per100k", pd.Series({2040: 0.}), stdev=10.) # used to minimise indicence
+        est.NormalTarget(minimised_indicator, pd.Series({2040: 0.}), stdev=10.) # used to minimise indicence
     ]
 
     # define another target to censor decision variables above a given threshold
@@ -34,4 +37,18 @@ def get_optimisation_bcm(fixed_params, decision_var_sum_threshold= 1.5) -> Bayes
         )
     )
 
-    return BayesianCompartmentalModel(model, fixed_params, priors, targets)
+    return BayesianCompartmentalModel(model, interv_params, priors, targets)
+
+
+def optimise_interventions(mle_params, decision_var_sum_threshold=1.5, minimised_indicator="incidence_per100k"):
+    with open(FIXED_PARAMS_PATH, "r") as f:
+        fixed_params = yaml.safe_load(f)
+
+    interv_params = fixed_params | {"use_interventions":True, "fitted_effective_contact_rate": mle_params["effective_contact_rate"]}
+    opti_bcm = get_optimisation_bcm(interv_params, decision_var_sum_threshold, minimised_indicator)
+
+    opti_orunner = optimize_model(opti_bcm, num_workers=8)
+    opti_rec = opti_orunner.minimize(1000)
+    opti_mle_params = opti_rec.value[1]
+
+    return opti_bcm, opti_mle_params
